933c9acb1b7eff32438f1baf7b246789d44a7326
Fangshilin <1244187813@qq.com>
2022-07-03 11:17:17 +0800

    完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,11 @@ module.exports = {
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "space-before-function-paren": "off",
   },
 };
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  // babel node_moudules 进行转义，安全性， 缺点： 耗时
   transpileDependencies: true,
+
+  // 关闭eslint， build serve将不会进行eslint检查
+  lintOnSave: false,
+  // 书写原生的webpack配置项
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 8080,
+      host: "localhost",
+    },
+  },
 });
